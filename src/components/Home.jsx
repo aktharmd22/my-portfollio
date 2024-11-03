1bb33bee8415636ef61fb67d28eb125422f6d50f
aktharmd22 <mohammedakthar215@gmail.com>
2024-11-03 22:20:50 +0530

    Resolved merge conflicts2

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,10 @@ import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[url("https://media0.giphy.com/media/xVn3ZmKrKIOLS/giphy.webp?cid=ecf05e47qvqqvzm1b90yeteqiwaxgp128jnkdiag3xbvwvwn&ep=v1_gifs_search&rid=giphy.webp&ct=g")]'>
+    <div
+      name="home"
+      className='w-full h-screen bg-[url("https://media0.giphy.com/media/xVn3ZmKrKIOLS/giphy.webp?cid=ecf05e47qvqqvzm1b90yeteqiwaxgp128jnkdiag3xbvwvwn&ep=v1_gifs_search&rid=giphy.webp&ct=g")]'
+    >
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-blue-300'>Hi, my name is</p>
@@ -12,19 +15,17 @@ const Home = () => {
           Mohamed Akthar
         </h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
-<<<<<<< HEAD
           I'm a FullStack Web Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          
-        As a full-stack web developer with digital marketing skills, I create responsive, user-friendly applications with strong backend support. My experience in digital marketing ensures that my projects are optimized for SEO, analytics, and audience engagement, delivering both technical performance and impactful user experiences.
-=======
-          I'm a FrontEnd Developer.
-        </h2>
-        <p className='text-[#8892b0] py-4 max-w-[700px]'>
-          
-            As a frontend developer with expertise in crafting outstanding user interfaces, I specialize in constructing responsive web applications that deliver exceptional digital experiences, occasionally incorporating design elements into the process.
->>>>>>> 18d9e0c743b1d7fae7157c0b66ffa8fee8c51192
+          As a full-stack web developer with digital marketing skills, I create
+          responsive, user-friendly applications with strong backend support.
+          My experience in digital marketing ensures that my projects are
+          optimized for SEO, analytics, and audience engagement, delivering both
+          technical performance and impactful user experiences. I also
+          specialize in constructing responsive web applications that deliver
+          exceptional digital experiences, occasionally incorporating design
+          elements into the process.
         </p>
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-blue-600 hover:border-blue-600'>
